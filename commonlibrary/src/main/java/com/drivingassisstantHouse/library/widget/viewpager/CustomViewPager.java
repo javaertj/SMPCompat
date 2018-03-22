6bf6f15842a140c4830e0fd6092d2e7e54153754
yankebin <ykbdevelop@gmail.com>
2018-03-22 16:29:36 +0800

    比较大的一次调整 android support相关升级到27.0.2 retrofit 升级到2.4.0 rxjava rxandroid升级 ...

--- commonlibrary/src/main/java/com/drivingassisstantHouse/library/widget/viewpager/CustomViewPager.java
+++ commonlibrary/src/main/java/com/drivingassisstantHouse/library/widget/viewpager/CustomViewPager.java
@@ -17,7 +17,6 @@ import android.support.annotation.DrawableRes;
 import android.support.v4.os.ParcelableCompat;
 import android.support.v4.os.ParcelableCompatCreatorCallbacks;
 import android.support.v4.view.AccessibilityDelegateCompat;
-import android.support.v4.view.KeyEventCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.VelocityTrackerCompat;
@@ -43,7 +42,6 @@ import android.view.ViewParent;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.animation.Interpolator;
 import android.widget.Scroller;
-
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -2641,9 +2639,9 @@ public class CustomViewPager extends ViewGroup {
                     if (Build.VERSION.SDK_INT >= 11) {
                         // The focus finder had a bug handling FOCUS_FORWARD and FOCUS_BACKWARD
                         // before Android 3.0. Ignore the tab key on those devices.
-                        if (KeyEventCompat.hasNoModifiers(event)) {
+                        if (event.hasNoModifiers()) {
                             handled = arrowScroll(FOCUS_FORWARD);
-                        } else if (KeyEventCompat.hasModifiers(event, KeyEvent.META_SHIFT_ON)) {
+                        } else if (event.hasModifiers(KeyEvent.META_SHIFT_ON)) {
                             handled = arrowScroll(FOCUS_BACKWARD);
                         }
                     }
